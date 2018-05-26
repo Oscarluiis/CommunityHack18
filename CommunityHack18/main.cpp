694c849ec320014dfa1fe9fda78b55fe7ce6772c
Oscarluiis <tinocoscar11@unitec.edu>
2018-05-26 17:28:31 -0600

    Add Color...
    Mmmmm cooooloooor

--- CommunityHack18/main.cpp
+++ CommunityHack18/main.cpp
@@ -14,3 +14,34 @@ using namespace sf;
 // Incluir Recursos (no necesariamente en la carpeta de proyecto, se puede con el path)
 
 #include "ResourcePath.hpp"
+
+// Crear la ventana (w=h, h=b)
+
+const int W=800;
+const int H=600;
+
+//Velocidad a la que se va desplazar (nivel unico)
+
+int speed = 4;
+
+// Abrir ventana [h][b]
+
+bool field [W][H];
+
+// Hacer un Struct para los jugadores
+
+struct player{
+    
+    // Movimiento en x, en y y en direcciones
+   
+    int x,y,dir;
+    
+    // Colorear (se le debe asignar un color a cada jugador)
+    
+    Color color;
+    
+    // Clase para el color
+    player (Color c){
+        
+    }
+};
