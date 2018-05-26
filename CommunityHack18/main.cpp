c8f0cc0eb852b63f932b2c4ee32fc7534ada5d08
Oscarluiis <tinocoscar11@unitec.edu>
2018-05-26 17:08:45 -0600

    Basic
    Soso...

---
+++ CommunityHack18/main.cpp
@@ -0,0 +1,16 @@
+// Para el manejo de la libreria
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+// Para manejar el archivo
+#include <fstream>
+#include <sstream>
+
+// Para c++
+using namespace std;
+using namespace sf;
+
+// Incluir Recursos (no necesariamente en la carpeta de proyecto, se puede con el path)
+
+#include "ResourcePath.hpp"
